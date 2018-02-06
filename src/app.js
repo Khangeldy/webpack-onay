7e850bdd2bc32d0e0d88810328402919ae1f8085
Khangeldy <hongeldon@inbox.ru>
2018-02-06 14:02:45 +0600

    travis no-console fix

--- src/app.js
+++ src/app.js
@@ -1,3 +1,3 @@
 import './css/style.sss';
 
-console.log('%c Cәлем %c бағдарламадан', 'font-size: 20px;', 'color: tomato;');
+// console.log('%c Cәлем %c бағдарламадан', 'font-size: 20px;', 'color: tomato;');
